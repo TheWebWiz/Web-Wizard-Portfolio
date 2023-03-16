a67db1214d22b0ccee3de602e006d373904a044b
James <thewebwizard@outlook.com>
2023-03-16 13:27:31 -0400

    added picture gallery and info

--- src/App/About/About.js
+++ src/App/About/About.js
@@ -1,7 +1,43 @@
 import React from "react";
+import './About.css';
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <main>
+      <section>
+        <figure>
+          <img src="./images/meandtiffboating20120.jpg" alt="Me and My wife out on our boat on Lake Erie" />
+          <figcaption>
+            Me and my wife taking our boat out on Lake Erie in 2020. Boating is one of my favorite hobbies, there is nothing like being out on the water, and feeling the waves crash agents your haul.
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="./images/puppieschristmas2022.jpg" alt="A photo of me and my wives dogs" />
+          <figcaption>
+            Our puppies first christmas! On the left is my dog Rot(short for Rotheford), who is a Australian Shepard. While on the right is my wifes dog Luna, who is a mix between a Poodle and a German Shepard.
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="./images/me_on_boat.jpg" alt="Me on a boat on land" />
+          <figcaption>
+            This is my boat. It is named The Unrealable and takes on the Great Lakes with utter beauty. We have taken as far north as Lake Saint Clare, and have many more trips planned!
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="./images/puppies2022.jpg" alt="Me and My wives dogs resting in her truck" />
+          <figcaption>
+            One of the days I worked late at my previous job, my wife surprised me and the puppies with a surprise visit. They where just as happy as I was!
+          </figcaption>
+        </figure>
+        <figure>
+          <img src="./images/meandtiffchrismas2020.jpg" alt="Me and my wife at christmas time in 2020" />
+          <figcaption>
+            Me and my wife during Christmas time in 2020. I always have a smile when she next to me.
+          </figcaption>
+        </figure>
+      </section>
+    </main>
+  );
 }
 
 export default About;
